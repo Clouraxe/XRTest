1c61e65b8b145ea207ee8f52c8f17ff137a08298
clouraxe <clouraxe@gmail.com>
2025-01-28 15:50:52 +0300

    feat: added bouncyball bouciness

---
+++ Assets/Scripts/BouncyBall.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+public class BouncyBall : Item
+{
+    
+
+
+    // Start is called before the first frame update
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+       
+    }
+    
+    
+    
+    
+}
