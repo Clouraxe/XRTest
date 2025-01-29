839a8e9298194d4d72d1c3f7d548331a8462f4f8
clouraxe <clouraxe@gmail.com>
2025-01-29 16:05:03 +0300

    feat: added optional circular motion to Targets

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,13 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Api;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 
 public class Target : MonoBehaviour
 {
+    public enum MoveType {
+        Default,
+        Circular
+    }
+    
     [SerializeField] private Transform endPoint;
     [SerializeField] private float speed = 5f;
+    [SerializeField] private MoveType moveType;
+
+    [Header("Circle Movement")]
+    [SerializeField] private Axis circleAround; //For circular motion
+    [SerializeField] private float circleRadius;
 
     private Vector3 startPos;
     private Vector3 endPos;
@@ -28,6 +42,28 @@ public class Target : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch(moveType) {
+            case MoveType.Default:
+                MoveLinear();
+                break;
+            
+            case MoveType.Circular:
+                MoveCircle();
+                break;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Item>() == null) return;
+        
+        OnPop?.Invoke();
+        Destroy(gameObject);
+    }
+    
+    
+    private void MoveLinear()
+    {
         if (endPos == null) return;
         
         var dest = isGoingBack ? startPos : endPos;
@@ -40,10 +76,37 @@ public class Target : MonoBehaviour
         }
         else isGoingBack = !isGoingBack; 
     }
+    
+    private void MoveCircle()
+    {
+        Vector3 newPos = transform.position;
+        float x, y, z;
+        switch(circleAround) {
+            case Axis.X:
+                y = startPos.y + Mathf.Cos(Time.time * speed) * circleRadius;
+                z = startPos.z + Mathf.Sin(Time.time * speed) * circleRadius;
+                newPos = new(startPos.x, y, z);
+                break;
+            
+            case Axis.Y:
+                x = startPos.x + Mathf.Cos(Time.time * speed) * circleRadius;
+                z = startPos.z + Mathf.Sin(Time.time * speed) * circleRadius;
+                newPos = new(x, startPos.y, z);
+                break;
+                
+            case Axis.Z:
+                x =  startPos.x + Mathf.Cos(Time.time * speed) * circleRadius;
+                y =  startPos.y + Mathf.Sin(Time.time * speed) * circleRadius;
+                newPos = new(x, y, startPos.z);
+                break;
+                
+            default:
+                Debug.LogError("No axis specified!!");
+                break;
+        }
 
-    public void OnTriggerEnter(Collider other)
-    {        
-        OnPop?.Invoke();
-        Destroy(gameObject);
+        transform.position = newPos; 
+        
+        
     }
 }
