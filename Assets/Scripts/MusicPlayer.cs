be6fcdb2acf212e4e9ba77ef8eb64de665aa9d2d
clouraxe <clouraxe@gmail.com>
2025-02-13 15:56:19 +0300

    feat: music and fridge bg

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public static MusicPlayer Instance;
+    private AudioSource _music;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (Instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        _music = GetComponent<AudioSource>();
+        SceneManager.activeSceneChanged += ChangedActiveScene;
+    }
+
+    private void ChangedActiveScene(Scene current, Scene next)
+    {
+        if (!next.name.ToLower().Contains("level")) {
+            _music.Stop();
+        } else if (next.name.ToLower().Contains("level 1") && !_music.isPlaying) {
+            _music.Play();
+        }
+
+
+    }
+}
